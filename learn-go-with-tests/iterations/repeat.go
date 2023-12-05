72571d62b0c76159b7ee626f7dd3914ebb8b10e9
Clément VIDON <cvidon@student.42.fr>
2023-12-05 16:41:59 +0100

    Add repeat

--- learn-go-with-tests/iterations/repeat.go
+++ learn-go-with-tests/iterations/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(character string) string {
-    var repeated string
-    for  i := 0; i < 5; i++ {
-        repeated = repeated + character
-    }
+func Repeat(times int, character string) string {
+	var repeated string
+	for i := 0; i < times; i++ {
+		repeated += character
+	}
 	return repeated
 }
--- learn-go-with-tests/iterations/repeat_test.go
+++ learn-go-with-tests/iterations/repeat_test.go
@@ -1,10 +1,23 @@
 package iteration
 
 import "testing"
+import "fmt"
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat(6, "a")
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat(3, "0")
+	fmt.Println(repeated)
+	// Output: 000
+}
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat(10, "a")
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
