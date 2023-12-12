a9c1e6ac992ace0b173f7a4af97ba8b77ae9388a
Clément VIDON <cvidon@student.42.fr>
2023-12-12 09:28:06 +0100

    Add sum

--- learn-go-with-tests/arrays-and-slices/sum_test.go
+++ learn-go-with-tests/arrays-and-slices/sum_test.go
@@ -1,17 +1,53 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSumAllTails(t *testing.T) {
+
+	// Hiding variales and functions that don't need to be exported is an important
+	// design consideration.
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
-		got := Sum(numbers)
-		want := 15
+	got := Sum(numbers)
+	want := 15
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
 }
