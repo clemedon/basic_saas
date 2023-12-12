28b7ee3ac5bc9d163fd3ec8914ace434914f2726
Clément VIDON <cvidon@student.42.fr>
2023-12-12 09:30:00 +0100

    Refactor shapes

--- learn-go-with-tests/structs-methods-interfaces/shapes_test.go
+++ learn-go-with-tests/structs-methods-interfaces/shapes_test.go
@@ -3,19 +3,58 @@ package main
 import "testing"
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
+
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12, 6}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
+
+	////////////////////////
+	// Table driven tests //
+	////////////////////////
+
+	// Run specific tests wihin the table:
+	// $ go test -run TestArea/Rectangle
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{1}, hasArea: 3.141592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g has area %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	hasArea := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	if got != hasArea {
+		t.Errorf("got %.2f has area %.2f", got, hasArea)
 	}
 }
