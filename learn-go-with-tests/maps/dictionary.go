78fb92c0f69bce0aea7a9dbbfc527faec64c9dfe
Clément VIDON <cvidon@student.42.fr>
2023-12-14 19:52:27 +0100

    GREEN Add existing words throws error

--- learn-go-with-tests/maps/dictionary.go
+++ learn-go-with-tests/maps/dictionary.go
@@ -4,8 +4,10 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExists = errors.New("the word you were looking for already exists")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("the word you were looking for already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	// maps can return two values including a found/not found boolean
@@ -16,6 +18,14 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	}
+	return nil
 }
